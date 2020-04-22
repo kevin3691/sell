31f1d98d413d23e19bc3520161e5583e5aaca9eb
kevin3691 <kevin3691@163.com>
2020-04-22 16:57:06 +0800

    修改商品名称a

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -2,7 +2,20 @@
   <div class="header">
     <div class="content-weapper">
       <div class="avatar">
-        <img  width="64" height="64" :src="seller.avatar" >
+        <img width="64" height="64" :src="seller.avatar">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
       </div>
     </div>
     <div class="bulletin-wrapper"></div>
@@ -11,15 +24,23 @@
 </template>
 
 <script>
-    export default {
-        props:{
-          seller:{
-            type: Object
-          }
+  export default {
+    props: {
+      seller: {
+        type: Object
       }
-    };
+    }
+  };
 </script>
 
-<style scoped>
-
+<style lang="stylus">
+  .header
+    color: #fff
+    /*background : #000*/
+    /*  .content-wrapper*/
+    /*    padding :24px 12px 18px 24px*/
+    /*    .avatar*/
+    /*      display: inline-block*/
+    /*    .content*/
+    /*      display: inline-block*/
 </style>
