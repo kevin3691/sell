4c35d866dfcbe4701cdb4b0b00eef586a0735897
kevin3691 <kevin3691@163.com>
2020-04-23 10:41:44 +0800

    把全部内容串上去a

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <div class="content-weapper">
+    <div class="content-wrapper">
       <div class="avatar">
         <img width="64" height="64" :src="seller.avatar">
       </div>
@@ -37,12 +37,13 @@
   .header
     color: #fff
     background: #000
-
     .content-wrapper
-      padding :24px 12px 18px 25px
-        .avatar
-          display: inline-block
-
-        .content
-          display: inline-block
+      padding : 24px 12px 18px 24px
+      font-size: 0
+      .avatar
+        display: inline-block
+      .content
+        display : inline-block
+        margin-left :16px
+        font-size: 14px
 </style>
